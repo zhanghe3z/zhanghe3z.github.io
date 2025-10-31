17d158d46934f36e2ae9e18e0033b75337047aed
zhanghe3z <54067097+zhanghe3z@users.noreply.github.com>
2025-10-31 16:18:12 +0000

    Deploying to gh-pages from @ zhanghe3z/zhanghe3z.github.io@ca9ba2e7ef3fff82d6174a8954cac444cccd72ef 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "zhanghe3z/zhanghe3z",
+    "data-repo": "zhanghe3z/zhanghe3z.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
