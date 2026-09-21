0c7a653e47ef2a502350ee6ced258c80d5a88f77
zhanghe3z <54067097+zhanghe3z@users.noreply.github.com>
2026-09-21 09:47:14 +0000

    Deploying to gh-pages from @ zhanghe3z/zhanghe3z.github.io@d55cffdb1b375a8c0bed7b7dfa5b775bec67785d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -109,6 +109,13 @@ ninja.data = [{
           window.open("/feed.xml", "_blank");
         },
       },{
+        id: 'social-scholar',
+        title: 'Google Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://scholar.google.com/citations?user=FUDsZkEAAAAJ", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
